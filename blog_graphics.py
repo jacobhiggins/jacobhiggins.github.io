e486aeb2ff69bd5a96371761d142f27588c52b7e
jacobhiggins <jdh4je@virginia.edu>
2020-07-10 17:05:57 -0400

    update

--- blog_graphics.py
+++ blog_graphics.py
@@ -17,13 +17,13 @@ ax = sns.heatmap(val,
                 linecolor='black',
                 cbar=False,
                 square=True,
-                annot=np.array([-1,10,10,10,10,10,10]).reshape(1,7),
+                annot=np.array([-1,-1,-1,-1,-1,-1,10]).reshape(1,7),
                 yticklabels=False)
 ax.set_xticklabels([1,2,3,4,5,6,7])
 for _,spine in ax.spines.items():
     spine.set_visible(True)
 
-plt.savefig("./images/blog_pics/RL1/fig4.png")
+plt.savefig("./images/blog_pics/RL1/fig5.png")
 
 plt.show()
 
